999d0bc7251906ebbb2feaf2832cf0679e00dc3f
dicksontan <dicksontan2618@gmail.com>
2023-12-13 23:00:28 +0800

    migrate prev jobs to react + nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,181 @@
+"use client"
+
 import Image from 'next/image'
+import { useEffect } from 'react';
+
 
 export default function Home() {
+
+  useEffect(() => {
+    const vendorData = {
+      vendor1: [
+        { id: 1, name: "Margherita Pizza", type: "pizza", price: 10.99 },
+        { id: 2, name: "Classic Burger", type: "burger", price: 8.99 },
+      ],
+      vendor2: [
+        { id: 3, name: "Salmon Sushi", type: "sushi", price: 12.99 },
+        // Add more items for vendor2
+      ],
+      // Add more vendors as needed
+    };
+
+    const vendorSelect = document.getElementById("vendor-select");
+    const searchInput = document.getElementById("search-input");
+    const foodFilter = document.getElementById("food-filter");
+    const foodList = document.getElementById("food-list");
+    const cartItems = document.getElementById("cart-items");
+    const cartTotal = document.getElementById("cart-total");
+    const locationInput = document.getElementById("location-input");
+
+    let cart = [];
+
+    // Function to render all food items initially
+    function renderAllFoodItems() {
+      foodList.innerHTML = "";
+
+      // Loop through all vendors and render their items
+      Object.values(vendorData).forEach((vendorItems) => {
+        vendorItems.forEach((food) => {
+          const foodItem = document.createElement("div");
+          foodItem.classList.add("food-item");
+          foodItem.innerHTML = `
+              <h3>${food.name}</h3>
+              <p>${food.type}</p>
+              <p>$${food.price.toFixed(2)}</p>
+              <button onclick="addToCart(${food.id}, '${
+            food.vendor
+          }')">Add to Cart</button>
+          `;
+          foodList.appendChild(foodItem);
+        });
+      });
+    }
+
+    // Function to render food items based on search, filter, and vendor
+    function renderFoodItems() {
+      const searchTerm = searchInput.value.toLowerCase();
+      const filterType = foodFilter.value;
+      const selectedVendor = vendorSelect.value;
+
+      // Get food items based on selected vendor
+      const vendorFood =
+        selectedVendor === "all"
+          ? Object.values(vendorData).flat()
+          : vendorData[selectedVendor] || [];
+
+      const filteredFood = vendorFood.filter((food) => {
+        const matchesSearch = food.name.toLowerCase().includes(searchTerm);
+        const matchesFilter = filterType === "all" || food.type === filterType;
+        return matchesSearch && matchesFilter;
+      });
+
+      foodList.innerHTML = "";
+
+      filteredFood.forEach((food) => {
+        const foodItem = document.createElement("div");
+        foodItem.classList.add("food-item");
+        foodItem.innerHTML = `
+          <h3>${food.name}</h3>
+          <p>${food.type}</p>
+          <p>$${food.price.toFixed(2)}</p>
+          <button onclick="addToCart(${food.id}, '${
+          food.vendor
+        }')">Add to Cart</button>
+      `;
+        foodList.appendChild(foodItem);
+      });
+    }
+
+    // Function to add item to the cart
+    function addToCart(foodId, vendor) {
+      const selectedFood = vendorData[vendor].find(
+        (food) => food.id === foodId
+      );
+
+      if (selectedFood) {
+        cart.push(selectedFood);
+        updateCart();
+      }
+    }
+
+    // Function to update the cart
+    function updateCart() {
+      cartItems.innerHTML = "";
+      let total = 0;
+
+      cart.forEach((item) => {
+        const cartItem = document.createElement("li");
+        cartItem.innerHTML = `${item.name} - $${item.price.toFixed(2)}`;
+        cartItems.appendChild(cartItem);
+        total += item.price;
+      });
+
+      cartTotal.textContent = total.toFixed(2);
+    }
+
+    // Function to save the user's location
+    function saveLocation() {
+      const userLocation = locationInput.value;
+      // You can use the user's location for various purposes (e.g., filtering restaurants based on location)
+      console.log("User Location:", userLocation);
+    }
+
+    // Event listeners
+    searchInput.addEventListener("input", renderFoodItems);
+    foodFilter.addEventListener("change", renderFoodItems);
+    vendorSelect.addEventListener("change", renderFoodItems);
+
+    // Initial render
+    renderAllFoodItems();
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+    <main className="">
+      <header>
+        <h1>Food Delivery App</h1>
+      </header>
+
+      <div id="location-section">
+        <label htmlFor="location-input">Enter Your Location:</label>
+        <input type="text" id="location-input" placeholder="E.g., Your City" />
+        <button onClick="saveLocation()">Save Location</button>
+      </div>
+
+      <div id="vendor-section">
+        <label htmlFor="vendor-select">Select Vendor:</label>
+        <select id="vendor-select">
+          <option value="all">All Vendors</option>
+          <option value="vendor1">Vendor 1</option>
+          <option value="vendor2">Vendor 2</option>
+          {/* Add more options as needed */}
+        </select>
+      </div>
+
+      <div id="search-bar">
+        <input type="text" id="search-input" placeholder="Search for food..." />
+        <select id="food-filter">
+          <option value="all">All</option>
+          <option value="pizza">Pizza</option>
+          <option value="burger">Burger</option>
+          <option value="sushi">Sushi</option>
+          {/* Add more options as needed */}
+        </select>
+      </div>
+
+      <div id="food-list">
+        {/* Food items will be dynamically added here using JavaScript */}
+      </div>
+
+      <div id="shopping-cart">
+        <h2>Shopping Cart</h2>
+        <ul id="cart-items">
+          {/* Food items will be dynamically added here using JavaScript */}
+        </ul>
+        <p>
+          Total: $<span id="cart-total">0.00</span>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
+
