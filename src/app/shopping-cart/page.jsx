e4ed8c0ce87a8a83fcf917192c4a3d16aa0202c8
dicksontan <dicksontan2618@gmail.com>
2023-12-18 07:44:36 +0800

    added layout to shopping cart, change files to .jsx

--- src/app/shopping-cart/page.jsx
+++ src/app/shopping-cart/page.jsx
@@ -6,15 +6,10 @@ const ShoppingCartPage = () => {
   const cart = []; // Update this with your actual cart data
 
   return (
-    <main>
-      <div className="hidden md:flex md:justify-center md:items-center md:w-screen md:h-screen ">
-        <h1>Application only available in mobile view!</h1>
-      </div>
-      <div className="block md:hidden">
-        <h1>Shopping Cart Page</h1>
-        <ShoppingCart cart={cart} />
-      </div>
-    </main>
+    <div>
+      <h1>Shopping Cart Page</h1>
+      <ShoppingCart cart={cart} />
+    </div>
   );
 };
 
