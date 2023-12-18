5169f53cfe25f7dc15bc87dfadfff5134f72a1f3
dicksontan <dicksontan2618@gmail.com>
2023-12-18 14:11:57 +0800

    minor amendments on project structure

--- src/app/shopping-cart/page.jsx
+++ src/app/shopping-cart/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ShoppingCart from "../components/ShoppingCart"; // Update the path accordingly
+import ShoppingCart from "../_components/ShoppingCart"; // Update the path accordingly
 
 const ShoppingCartPage = () => {
   // Assuming you have a way to get the cart state, you might need to fetch it or pass it as a prop
