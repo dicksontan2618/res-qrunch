d384f28116329f0c5ce28b2952a50b1689f44c06
dicksontan <dicksontan2618@gmail.com>
2023-12-18 15:12:55 +0800

    import daisyui, added startscreen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,18 @@ import Link from 'next/link';
 
 const StartScreen = () => {
     return(
-        <div>
-            <p>Test</p>
+        <div className='bg-main-clr h-screen flex justify-center items-center'>
+            <div className='flex flex-col items-center gap-y-8'>
+                {/* TODO: Insert Logo Image here  */}
+                <p className='text-4xl font-semibold text-white'>ResQrunch</p>
+                <div className=''>
+                    <Link href="./home">
+                        <button className='btn btn-wide bg-white text-main-clr text-md border-white'>
+                            Get Started!
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
