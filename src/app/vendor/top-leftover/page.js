991c3632d7663e6152b5c1874850b08c3617ac0b
dicksontan <dicksontan2618@gmail.com>
2024-01-25 13:36:14 +0800

    analytics part - dynamic sales and leftover analysis

--- src/app/vendor/top-leftover/page.js
+++ src/app/vendor/top-leftover/page.js
@@ -1,58 +1,101 @@
 "use client"
-import React, { useState } from 'react';
 
-const DonationSummary = () => {
-  const [selectedMonth, setSelectedMonth] = useState('WeekTotal1');
-  var selectedItem = "Chicken Burger";
+import { useState, useEffect } from "react";
+import { useAuthContext } from "@/context/AuthContextVendor";
+import { useRouter } from "next/navigation";
 
-  const handleMonthChange = (event) => {
-    setSelectedMonth(event.target.value);
-  };
+import { db } from "@/utils/firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-  const getChartForMonth = (month) => {
-    var link = "/" + month + ".png";
-    return link;
-  };
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-  const getItem = (month) => {
-    if(month == "WeekTotal1"){
-      selectedItem = "Chicken Burger";
-    }else if(month == "WeekTotal4"){
-      selectedItem = "Beef Burger";
-    }else if(month == "WeekTotal3"){
-      selectedItem = "Fried Chicken & Bubble Milk Tea";
-    }else if(month == "WeekTotal2"){
-      selectedItem = "Chicken Burger";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const colorsList = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(73, 12, 212, 0.2)",
+  "rgba(175, 2, 81, 0.2)",
+  "rgba(23, 31, 92, 0.2)",
+  "rgba(46, 123, 166, 0.2)",
+  "rgba(35, 222, 1777, 0.2)",
+  "rgba(91, 129, 188, 0.2)",
+  "rgba(88, 113, 199, 0.2)",
+  "rgba(66, 211, 200, 0.2)",
+  "rgba(23, 19, 201, 0.2)",
+  "rgba(22, 92, 122, 0.2)",
+  "rgba(70, 12, 222, 0.2)",
+];
+
+const LeftoverSummary = () => {
+
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  const [data,setData] = useState([])
+  const [maxLeftover, setMaxLeftover] = useState([])
+
+  const getVendorLeftovers = async () => {
+
+    let tempLabelList = []
+    let tempQtyList = []
+
+    let tempMaxIndex = 0
+
+    const q = query(
+      collection(db, "menuItems"),
+      where("vendor", "==", user.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.docs.forEach((doc) => {
+      tempLabelList.push(doc.data()["name"])
+      tempQtyList.push(Number(doc.data()["ori_quantity"]))
+    })
+
+    tempMaxIndex = tempQtyList.indexOf(Math.max(...tempQtyList));
+    setMaxLeftover(tempLabelList[tempMaxIndex]);
+
+    renderChart(tempLabelList,tempQtyList);
+  }
+
+  const renderChart = (label,qt) => {
+    let tempData = {
+      labels: label,
+      datasets: [
+        {
+          label: "Number of Leftovers",
+          data: qt,
+          backgroundColor: colorsList.slice(0,label.length),
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    setData(tempData);
+  }
+
+  useEffect(() => {
+    if (
+      user == null &&
+      window.localStorage.getItem("session_user") !== "vendor"
+    ) {
+      router.push("/");
     }
-    return selectedItem;
-  };
+  }, [user]);
+
+  useEffect(() => {
+    getVendorLeftovers();
+  },[])
 
   return (
-    <div>
-      <div id="sticky-banner" tabindex="-1" class="relative top-0 start-0 z-50 flex justify-between w-full p-4 border-b border-gray-200 bg-white-50 dark:bg-gray-700 dark:border-gray-600 shadow-xl">
-        <div class="flex justify-between mx-auto">
-        <label htmlFor="monthDropdown">Select Month: </label>
-          <select
-            id="monthDropdown"
-            value={selectedMonth}
-            onChange={handleMonthChange}
-          >
-            <option value="WeekTotal1">October 2023</option>
-            <option value="WeekTotal2">November 2023</option>
-            <option value="WeekTotal3">December 2023</option>
-            <option value="WeekTotal4">January 2024</option>
-          </select>
-        </div>
-    </div>
-
-      {/* Body of the Interface */}
-      <div className="flex justify-center items-center flex-col">
-        <h2 className="mt-6 mb-3 font-medium text-center">Most Over Prepared Food</h2>
-        <h3 className="mb-5 font-semibold text-red-500 bg-pink-100 p-2 rounded">{getItem(selectedMonth)}</h3>
-        <img src={getChartForMonth(selectedMonth)} alt={`Chart for ${selectedMonth}`} />
-      </div>
+    <div className="mt-24 flex flex-col justify-center">
+      {data.labels && <Pie data={data} />}
+      <p className="text-center font-bold mt-8">Max Leftover Item is: {maxLeftover}</p>
     </div>
   );
 };
 
-export default DonationSummary;
+export default LeftoverSummary;
